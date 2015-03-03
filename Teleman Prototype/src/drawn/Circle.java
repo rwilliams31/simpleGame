51c85f34c0ecf8eeb80f8114c0fe06abb4337376
Mammoth <rwilliams31@users.noreply.github.com>
2015-03-02 22:04:30 -0500

    Created Eclipse Package Wrote drawables package Wrote Teleman and telemanprotoplay
    This is the original commit (after updating readme)

---
+++ Teleman Prototype/src/drawn/Circle.java
@@ -0,0 +1,53 @@
+package drawn;
+import java.awt.Color;
+
+
+public class Circle extends DrawnObject{
+
+	private int radius;
+	
+	public Circle(){
+		super();
+		radius=10;
+	}
+	public Circle(int xi, int yi){
+		super(xi, yi);
+		radius=10;
+	}
+	public Circle(int xi, int yi, Color c){
+		super(xi, yi, c);
+		radius=10;
+	}
+	public Circle(int xi, int yi, int r){
+		super(xi, yi);
+		radius=r;
+	}
+	public Circle(int xi, int yi, int r, Color c){
+		super(xi, yi, c);
+		radius=r;
+	}
+
+	@Override
+	public void draw() {
+		Color tmp = StdDraw.getPenColor();
+		StdDraw.setPenColor(color);
+		if(filled){
+			StdDraw.filledCircle(x, y, radius);
+		}
+		else{
+			StdDraw.circle(x, y, radius);
+		}
+		StdDraw.setPenColor(tmp);
+	}
+	
+	public int getRadius(){
+		return radius;
+	}
+	public void setRadius(int r){
+		radius=r;
+	}
+
+	public boolean collidesWith(Circle other){
+		return Math.sqrt((this.x-other.x)*(this.x-other.x) - (this.y-other.y)*(this.y-other.y)) <= (this.radius+other.radius);
+	}
+}
